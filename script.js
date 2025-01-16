42fccd83e47555e12728e6f9eecaa6663d1bfd6f
ENAA <khadijauser779@gmail.com>
2025-01-16 18:00:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() { 
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+
+
+    $(window).scroll(function() {
+        $('.service-card').each(function() {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).addClass('animate__animated animate__fadeInUp');
+            }
+        });
+    });
+});
